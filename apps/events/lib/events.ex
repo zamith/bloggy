c8346b752e3479e7459a9e799c71a1f5b26cd854
Zamith <zamith.28@gmail.com>
2017-10-23 23:03:19 +0100

    Adds events based on event storming session
    Why:
    
    * An event sourced application needs to have events. Furthermore, it is
      benefial if those events are global so that they can be shared across
      bounded contexts, which is something that makes sense a lot of times
    
    This change addresses the need by:
    
    * Adding the events (basically structs) to a specific app in the
      umbrella so that others apps (which represent bounded contexts in the
      DDD sense) can require and use them.
      The data on each event is tentative and can (will?) change in the
      future, particularly it is not possible at this moment to edit the
      author, because we'll be running under the assumption that this will
      not be a valid scenario.

--- apps/events/lib/events.ex
+++ apps/events/lib/events.ex
@@ -1,2 +1,6 @@
 defmodule Bloggy.Events do
+  defmodule DraftCreated, do: defstruct [:article_id, :title, :body, :author_id]
+  defmodule ArticlePublished, do: defstruct [:article_id, :slug]
+  defmodule ArticleUnpublished, do: defstruct [:article_id]
+  defmodule ArticleEdited, do: defstruct [:article_id, :title, :body]
 end
