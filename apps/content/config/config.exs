d274ab8597a425a4127a32dc1eb688d1801fbd0d
Zamith <zamith.28@gmail.com>
2017-10-23 23:16:06 +0100

    Configures the event store and ecto
    Why:
    
    * Both need to be configured in how to connect to the external database
    
    This change addresses the need by:
    
    * Configuring the event store to use a local database for development
      (we'll configure production later on, when deploying)
    * Configuring ecto to use the same database. In order to do this, we
      resort to a small "trick", which is having separate schema migrations
      tables for ecto and the event store, so that they can run in parallel
      without affecting one another

--- apps/content/config/config.exs
+++ apps/content/config/config.exs
@@ -1,30 +1,19 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# This configuration is loaded before any dependency and is restricted
-# to this project. If another project depends on this project, this
-# file won't be loaded nor affect the parent project. For this reason,
-# if you want to provide default values for your application for
-# 3rd-party users, it should be done in your "mix.exs" file.
+config :commanded,
+  event_store_adapter: Commanded.EventStore.Adapters.EventStore
 
-# You can configure your application as:
-#
-#     config :content, key: :value
-#
-# and access this configuration in your application as:
-#
-#     Application.get_env(:content, :key)
-#
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+config :eventstore, EventStore.Storage,
+  serializer: Commanded.Serialization.JsonSerializer,
+  database: "bloggy_dev",
+  hostname: "localhost",
+  pool_size: 10
 
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+config :content, ecto_repos: [Bloggy.Content.Repo]
+
+config :content, Bloggy.Content.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "bloggy_dev",
+  hostname: "localhost",
+  pool_size: 10,
+  migration_source: "bloggy_schema_migrations"
---
+++ apps/content/lib/content/repo.ex
@@ -0,0 +1,3 @@
+defmodule Bloggy.Content.Repo do
+  use Ecto.Repo, otp_app: :content
+end
