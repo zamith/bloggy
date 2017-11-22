d6b5696fcca56fc22c8be1d865509379b287a8e8
Zamith <zamith.28@gmail.com>
2017-11-22 19:57:28 +0000

    Changes body type on the DB to be text

--- apps/content/priv/repo/migrations/20171029161151_create_articles.exs
+++ apps/content/priv/repo/migrations/20171029161151_create_articles.exs
@@ -5,7 +5,7 @@ defmodule Bloggy.Content.Repo.Migrations.CreateArticles do
     create table("articles", primary_key: false) do
       add :article_id, :uuid, primary_key: true
       add :title, :string, null: false
-      add :body, :string, null: false
+      add :body, :text, null: false
       add :author_id, :binary_id, null: false
       add :slug, :string
 
